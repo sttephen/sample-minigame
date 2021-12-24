3cec7e1f6aa92b58fee7dedf472e48254f56c1da
BuildTools <unconfigured@null.spigotmc.org>
2021-12-24 01:17:49 +0000

    Removed Ending gamestate - was unused

--- src/main/java/com/stephen/minigame/command/ArenaCommand.java
+++ src/main/java/com/stephen/minigame/command/ArenaCommand.java
@@ -51,7 +51,7 @@ public class ArenaCommand implements CommandExecutor {
 
                 if (id >= 0 && id <= minigame.getArenaManager().getArenas().size()) {
                     Arena arena = minigame.getArenaManager().getArena(id);
-                    if (arena.getState().equals(GameState.RECRUITING)) {
+                    if (arena.getState().equals(GameState.RECRUITING) || arena.getState().equals(GameState.COUNTDOWN)) {
                         player.sendMessage(ChatColor.GREEN + "You are now playing in Arena " + id + ".");
                         arena.addPlayer(player);
                     } else {
